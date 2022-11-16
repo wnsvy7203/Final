a03fd45f62d94182dd8cb5da139cdcc0a5d803ae
wnsvy7203 <wnsvy7203@gmail.com>
2022-11-16 13:52:12 +0900

    fixtures

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -1,18 +1,22 @@
 from django.db import models
-from django.conf import settings
 
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Movie(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    content = models.TextField()
-    released_data = models.DateField()
-    popularity = models.FloatField()
-    vote_avg = models.FloatField()
+    released_date = models.DateField()
+    vote_count = models.IntegerField()
+    vote_average = models.FloatField()
     overview = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    poster_path = models.TextField()
+    genres = models.ManyToManyField(Genre)
 
 
 class Comment(models.Model):
--- final-pjt-back/movies/serializers.py
+++ final-pjt-back/movies/serializers.py
@@ -7,7 +7,7 @@ class MovieListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Movie
-        fields = ('id', 'title', 'content', 'user', 'username')
+        fields = ('id', 'title')
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -21,12 +21,10 @@ class CommentSerializer(serializers.ModelSerializer):
 class MovieSerializer(serializers.ModelSerializer):
     comment_set = CommentSerializer(many=True, read_only=True)
     comment_count = serializers.IntegerField(source='comment_set.count', read_only=True)
-    username = serializers.CharField(source='user.username', read_only=True)
 
     class Meta:
         model = Movie
         fields = '__all__'
-        read_only_fields = ('user', )
 
 
 
