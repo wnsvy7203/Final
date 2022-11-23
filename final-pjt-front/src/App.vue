f076009b97175c3d53e265e5ab6d6e9de0586bd9
wnsvy7203 <wnsvy7203@gmail.com>
2022-11-23 11:37:52 +0900

    happy

--- final-pjt-front/src/App.vue
+++ final-pjt-front/src/App.vue
@@ -1,44 +1,43 @@
 <template>
   <div id="app">
-    <nav class="navbar justify-content-end navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        
-        <router-link :to="{ name: 'movie' }" class="navbar-brand" style="font-size: 50px">Best Film</router-link>
-        
-        <router-link :to="{ name: 'search' }" class="navbar-brand" style="font-size: 50px"> Search </router-link>
-        
-        <b-dropdown v-if="isLogin===true"
-            id="dropdown-dropleft"
-            dropleft text="Drop-Left"
-            size="lg"
-            variant="link"
-            toggle-class="text-decoration-none"
-        >
-          <template #button-content>
-            <span class="navbar-toggler-icon"></span>
-          </template>
-            <b-dropdown-item-button><router-link :to="{ name: 'profile' }" class="dropdown-item"> Profile </router-link></b-dropdown-item-button>
-            <b-dropdown-divider></b-dropdown-divider>
-            <b-dropdown-item-button @click="logOut"> Logout </b-dropdown-item-button>
-        </b-dropdown>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-navbar-brand>
+        <router-link :to="{ name: 'movie' }" class="navbar-brand" style="font-size: 50px">SIXth Sense</router-link>
+      </b-navbar-brand>
 
-        <b-dropdown v-else
-          id="dropdown-dropleft"
-          dropleft text="Drop-Left"
-          size="lg"
-          variant="link"
-          toggle-class="text-decoration-none"
-        >
-          <template #button-content>
-            <span class="navbar-toggler-icon"></span>
-          </template>
-          <b-dropdown-item-button><router-link :to="{ name: 'login' }" class="dropdown-item"> Login </router-link></b-dropdown-item-button>
-          <b-dropdown-divider></b-dropdown-divider>
-          <b-dropdown-item-button><router-link :to="{ name: 'signup' }" class="dropdown-item"> Signup </router-link></b-dropdown-item-button>
-        </b-dropdown>
-      </div>
-      
-    </nav>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">SEARCH</b-button>
+          </b-nav-form>
+
+          <b-nav-item-dropdown
+            right
+            v-if="isLogin === true"
+          >
+            <template #button-content>
+              <em>user</em>
+            </template>
+            <b-dropdown-item><router-link :to="{ name: 'profile' }" class="dropdown-item"> 내 프로필 </router-link></b-dropdown-item>
+            <b-dropdown-item @click="logOut"> 로그아웃</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item-dropdown
+            right
+            v-else
+          >
+            <template #button-content>
+              <em>user</em>
+            </template>
+            <b-dropdown-item><router-link :to="{ name: 'signup' }" class="dropdown-item"> 회원가입 </router-link></b-dropdown-item>
+            <b-dropdown-item><router-link :to="{ name: 'login' }" class="dropdown-item"> 로그인 </router-link></b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
     <router-view/>
     <v-app>
         <MyComponent/>
